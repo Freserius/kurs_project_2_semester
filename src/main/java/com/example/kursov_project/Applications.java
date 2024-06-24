c0969e6b4c9a9ce223ba8d6ecf3aa53e692bdcad
Ivan Fediunin <fediuninim@gmail.com>
2024-06-24 10:25:21 +0300

    Changed title of program and maked project .jar

--- src/main/java/com/example/kursov_project/Applications.java
+++ src/main/java/com/example/kursov_project/Applications.java
@@ -12,7 +12,7 @@ public class Applications extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Applications.class.getResource("auteurist.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 330, 300);
-        stage.setTitle("Hello!");
+        stage.setTitle("Restoran System");
         stage.setScene(scene);
         stage.show();
     }
